a00ebf16171fc80b1b638bed740d11d74a73e51b
Hamza <jhamzaahmed@gmail.com>
2024-01-17 19:28:34 +0500

    using GET method

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("hi there");
+  // res.send("hi there");
+  // res.sendStatus(200);
+  // res.status(200).send("hi there");
+  // res.json({ message: "success" });
+  // res.download("script.js");
+
+  res.render("/views/index.html");
 });
 
 app.listen(port, () => console.log(`server started at port ${port}`));
